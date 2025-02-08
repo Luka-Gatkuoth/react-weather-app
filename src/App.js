52c8641c6975e250d52d8477f0f0af2d9754ff8d
= <=>
2025-02-08 20:27:15 +0300

    structure of components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import hotImage from "./assets/hot.jpg";
+import coldImage from "./assets/cold.jpg";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, world</h1>
+    <div className="App" style={{background: `url(${hotImage})`}}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section__input">
+            <input type="text" name="city" placeholder="Enter the city"/>
+            <button>°F</button>
+          </div>
+          <div className="section section__temperature">
+            <div className="icon">
+              <h3>London, GB</h3>
+              <p>date</p>
+              {/* add image here from open weather api */}
+              <img src="" alt="weatherIcon"/>
+              <h3>Cody</h3>
+            </div>
+            <diV className="temperature">
+              <h2>34°</h2>
+            </diV>
+          </div>
+          {/* bottom description */}
+          
+        </div>
+      </div>
     </div>
   );
 }
