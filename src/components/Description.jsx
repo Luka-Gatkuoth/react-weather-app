3fc5fe44d80d737e095d186e417aebf8631ece94
= <=>
2025-02-09 13:34:05 +0300

    card data

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -4,7 +4,41 @@ import { BiHappy } from "react-icons/bi";
 import { MdCompress, MdOutlineWaterDrop } from "react-icons/md";
 import "./description.css"
 
-const Description = () => {
+const Description = (weather, units) => {
+  const tempUnit = units === "metric" ? "°C" : "°F";
+  const windUnit = units === "metric" ? "m/s" : "m/h";
+
+  // card data
+  const cards = [
+    {
+      id: 1,
+      icon: <BiHappy />,
+      title: "feels like",
+      data: weather.feels_like.toFixed(),
+      unit: tempUnit,
+    },
+    {
+      id: 2,
+      icon: <MdCompress />,
+      title: "pressure",
+      data: weather.pressure,
+      unit: "hPa",
+    },
+    {
+      id: 3,
+      icon: <MdOutlineWaterDrop />,
+      title: "humidity",
+      data: weather.humidity,
+      unit: "%",
+    },
+    {
+      id: 4,
+      icon: <FaWind />,
+      title: "wind speed",
+      data: weather.speed.toFixed(),
+      unit: windUnit,
+    },
+  ];
   return (
     <div className='section section__description'>
       <div className="card">
