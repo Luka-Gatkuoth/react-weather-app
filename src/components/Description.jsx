215605c9aced6c60f2c4ab42cb494d7587a2de54
= <=>
2025-02-09 09:59:24 +0300

    description styles

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaArrowDown } from "react-icons/fa";
+import "./description.css"
 
 const Description = () => {
   return (
@@ -7,7 +8,23 @@ const Description = () => {
       <div className="card">
         <div className="description__card-icon">
         <FaArrowDown />
+        <small>Min</small>
         </div>
+        <h2>32°C</h2>
+      </div>
+      <div className="card">
+        <div className="description__card-icon">
+        <FaArrowDown />
+        <small>Min</small>
+        </div>
+        <h2>32°C</h2>
+      </div>
+      <div className="card">
+        <div className="description__card-icon">
+        <FaArrowDown />
+        <small>Min</small>
+        </div>
+        <h2>32°C</h2>
       </div>
     </div>
   )
